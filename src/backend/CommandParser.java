d46538a70ecc6b4f91b7efd9239e8087b276699b
GAURAV KUMAR <gaurav.s.kumar3@gmail.com>
2016-02-23 21:33:53 -0500

    backend class shells

---
+++ src/backend/CommandParser.java
@@ -0,0 +1,37 @@
+package backend;
+
+public class CommandParser {
+	
+	private String myCommand;
+	private String myLanguage;
+	private Parameters myParameters;
+
+	public CommandParser() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void parse(String command) {
+		// TODO put it all together
+	}
+	
+	private void inputCommand(String command) {
+		myCommand = command;
+	}
+	
+	public void setLanguage(String language) {
+		myLanguage = language;
+	}
+	
+	private String parseCommand(String command) {
+		// TODO search through properties file to find command
+	}
+	
+	public Parameters getParameters() {
+		return myParameters;
+	}
+	
+	private void throwError() {
+		// TODO throw an error 
+	}
+
+}
