7eb7b0fce2a2bad12da6114a959e47bc604e5f0a
Joseph Lilien <jrl48@duke.edu>
2016-02-26 20:39:37 -0500

    quick fix

---
+++ src/frontend/Entry.java
@@ -0,0 +1,12 @@
+package frontend;
+
+public interface Entry {
+    
+    public Object getFirstValue ();
+
+    public void setFirstValue (Object command);
+
+    public Object getSecondValue ();
+
+    public void setSecondValue (Object result);
+}
