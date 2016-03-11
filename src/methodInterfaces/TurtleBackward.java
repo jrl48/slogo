4c79ba229ce8d90104c9f21ed6736ecb8527f8fb
vcheng19 <vc54@duke.edu>
2016-03-11 12:54:49 -0500

    Pushing to pull

--- src/methodInterfaces/TurtleBackward.java
+++ src/methodInterfaces/TurtleBackward.java
@@ -1,11 +1,12 @@
 package methodInterfaces;
 import frontend.Display;
+import frontend.SingleTurtle;
 
 public class TurtleBackward implements TurtleInterface {
 
 	@Override
-	public double executeCommand(double[] args, Display display) {
-		display.moveTurtleForward(args[0]*(-1));
+	public double executeCommand(double[] args, SingleTurtle turtle) {
+		turtle.moveTurtleForward(args[0]*(-1));
 		return args[0];
 	}
 }
--- src/methodInterfaces/TurtlePenUp.java
+++ src/methodInterfaces/TurtlePenUp.java
@@ -4,8 +4,8 @@ import frontend.Display;
 public class TurtlePenUp implements TurtleInterface {
 	
 	@Override
-	public double executeCommand(double[] args, Display display) {
-		display.turtlePenUp();
+	public double executeCommand(double[] args, SingleTurtle turtle) {
+		turtle.turtlePenUp();
 		return 0;
 	}
 	
